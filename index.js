b5cf6b84e72535487fe2f6601a68ed6d555cb1c4
subhan300 <subhan.akram2400@gmail.com>
2024-06-23 01:08:19 +0500

    status api added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const config = require('./config/default');
-const companyRoute=require("./routes/companyRoute")
+const companyRoute = require("./routes/companyRoute");
 const userRoute = require('./routes/userRoute');
-const stockItemRoute=require("./routes/stockItemsRoute")
-const testingRoute=require("./routes/testingRoute")
-const stockUsedInRoute=require("./routes/stockUsedInRoute")
-const ticketRoute=require("./routes/ticketRoute")
+const stockItemRoute = require("./routes/stockItemsRoute");
+const testingRoute = require("./routes/testingRoute");
+const stockUsedInRoute = require("./routes/stockUsedInRoute");
+const ticketRoute = require("./routes/ticketRoute");
 const connectDB = require('./config/db');
 
 dotenv.config();
@@ -20,13 +20,18 @@ connectDB();
 // Middleware
 app.use(express.json());
 
+// Health Check Route
+app.get('/', (req, res) => {
+  res.status(200).json({ status: 'UP' });
+});
+
 // Routes
 app.use('/api/users', userRoute);
 app.use('/api/companies', companyRoute);
-app.use('/api/stockItems',stockItemRoute);
-app.use('/api/testingRoute',testingRoute);
-app.use('/api/stockUsedInRoute',stockUsedInRoute);
-app.use('/api/ticket',ticketRoute);
+app.use('/api/stockItems', stockItemRoute);
+app.use('/api/testingRoute', testingRoute);
+app.use('/api/stockUsedInRoute', stockUsedInRoute);
+app.use('/api/ticket', ticketRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
