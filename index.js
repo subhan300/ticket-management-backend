c040e8688d429548d8786e290a26b3879a126b34
subhan300 <subhan.akram2400@gmail.com>
2024-08-05 03:23:03 +0500

    cors added

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const port = config.port;
 // Middleware
 app.use(express.json());
 const corsOptions = {
-  origin: "https://ticket-managment-frotnend.vercel.app", // allow all URLs
+  origin: ["https://ticket-managment-frotnend.vercel.app"], // allow all URLs
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // allow all methods\
   allowedHeaders: [
     'Content-Type',
