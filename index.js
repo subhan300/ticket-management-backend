fca11f1fdd673627131b7f2f7c36d7d989731b1d
subhan300 <subhan.akram2400@gmail.com>
2024-08-05 03:11:59 +0500

    cors added

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions))
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
