66993d08e2ac91411cc797dfaaab2d328cb831eb
subhan300 <subhan.akram2400@gmail.com>
2024-10-01 02:10:27 +0500

    room model type added

--- models/roomModel.js
+++ models/roomModel.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// Define the Unit schema
 const roomSchema = new Schema({
   SKU: { type: String, required: true },
   roomName: {
@@ -16,7 +15,8 @@ const roomSchema = new Schema({
   location: { type: Schema.Types.ObjectId, ref: "Location", required: true },
   userItems:[{type:Schema.Types.ObjectId,ref:"UserItem"}],
   tickets:[{type:Schema.Types.ObjectId,ref:"Ticket"}],
-  laundaryTickets:[{type:Schema.Types.ObjectId,ref:"LaundryTicket"}]
+  laundaryTickets:[{type:Schema.Types.ObjectId,ref:"LaundryTicket"}],
+  type:{type:String,default:"general",required:false}
 });
 
 // Create and export the Unit model
