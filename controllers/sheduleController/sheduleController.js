589134e7590090eb22f943e3608275c31b4b42c4
subhan300 <subhan.akram2400@gmail.com>
2024-10-29 05:16:02 +0500

    schedule events added

--- controllers/sheduleController/sheduleController.js
+++ controllers/sheduleController/sheduleController.js
@@ -1,23 +1,33 @@
 const Agenda = require("agenda");
 const Job = require("../../jobs/jobs");
 const dotenv = require("dotenv");
+const Ticket = require("../../models/ticketModel");
+const { formatTicketNumber } = require("../../utils");
 
 dotenv.config();
 const databaseUrl=process.env.MONGODB_URI
 console.log("dat base",databaseUrl)
 const agenda = new Agenda({ db: { address: databaseUrl } });
+const getLastTicketNumber = async () => {
+  const lastTicket = await Ticket.findOne().sort({ ticketNo: -1 });
+  const getNumber = lastTicket ? lastTicket.ticketNo : 0;
+  return formatTicketNumber(getNumber);
+};
 
 Job(agenda);
 agenda.start().then(() => {
     console.log('Agenda started');
   });
  const sheduleTicketCreation=async(req,res)=>{
-  
+    const { companyId, name, email, id: userId } = req.user;
         const { dateTime, data } = req.body;
+        const ticketNo = await getLastTicketNumber();
+
+        const payload={...data,ticketNo,userId,companyId,ticketNo}
       
         try {
-          const job = await agenda.schedule(new Date(dateTime), 'my job', {
-            data
+          const job = await agenda.schedule(new Date(dateTime), 'create ticket', {
+            data:payload
           });
           res.send(`Job scheduled with ID: ${job.attrs._id} for ${dateTime}`);
         } catch (error) {
--- jobs/jobs.js
+++ jobs/jobs.js
@@ -1,8 +1,39 @@
-module.exports = function (agenda) {
-    agenda.define("my job", async (job) => {
-      const data = job.attrs.data;
-      console.log("Job executed with data:", data);
-      // Add your job logic here
+const { getAllUsersByRole } = require("../controllers/globalController/GlobalController");
+const Ticket = require("../models/ticketModel");
+const { MANAGER, TECHNICIAN } = require("../utils/constants");
+const {
+    handleTicketNotification,
+  } = require("../controllers/notificationController/notificationHelper");
+const { formatTicketNumber } = require("../utils");
+
+module.exports = function (agenda,req) {
+    
+  agenda.define("create ticket", async (job) => {
+   try{
+    const { data } = job.attrs.data;
+    const { companyId, } = data;
+    console.log("Creating ticket with data:", data);
+    const ticket = new Ticket(data);
+    await ticket.save();
+    const technicians = await getAllUsersByRole(companyId, TECHNICIAN);
+    const managers = await getAllUsersByRole(companyId, MANAGER);
+    // handleTicketNotification(req, managers, technicians, ticket);
+    await ticket.populate({
+      path: "room",
+      populate: {
+        path: "unit",
+        model: "Unit",
+      },
     });
-  };
-  
+
+    console.log("Ticket created successfully");
+   }catch(err){
+    console.log(err);
+   }
+  });
+  agenda.define("my job", async (job) => {
+    const data = job.attrs.data;
+    console.log("Job executed with data:", data);
+    // Add your job logic here
+  });
+};
