cc6753c14d63f804b02dff05584413cdd7365da6
Subhan-Akram <subhan@denovers.com>
2024-12-03 22:59:22 +0500

    update: restriction admin removed

--- routes/companyRoute.js
+++ routes/companyRoute.js
@@ -1,19 +1,23 @@
 // companyRoutes.js
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const companyController = require('../controllers/companyController');
-const { adminAuthenticateJWT } = require('../middleware/adminAuthMiddleware');
-const {authenticateJWT}=require("../middleware/authMiddleware")
+const companyController = require("../controllers/companyController");
+const { adminAuthenticateJWT } = require("../middleware/adminAuthMiddleware");
+const { authenticateJWT } = require("../middleware/authMiddleware");
 // Route to get all companies
-router.get('/getAll', adminAuthenticateJWT, companyController.getAllCompanies);
+router.get("/getAll", authenticateJWT, companyController.getAllCompanies);
 
 // Route to get company by ID
-router.get('/:companyId', authenticateJWT, companyController.getCompanyById);
-router.get('/users/:companyId', authenticateJWT, companyController.getUsersByCompanyId);
-router.post('/create', adminAuthenticateJWT, companyController.createCompany);
-router.post('/:companyId/units', companyController.addUnit);
-router.post('/units/:unitId/rooms', companyController.addRoom);
+router.get("/:companyId", authenticateJWT, companyController.getCompanyById);
+router.get(
+  "/users/:companyId",
+  authenticateJWT,
+  companyController.getUsersByCompanyId
+);
+router.post("/create", authenticateJWT, companyController.createCompany);
+router.post("/:companyId/units", companyController.addUnit);
+router.post("/units/:unitId/rooms", companyController.addRoom);
 
 // Other routes like POST /companies (create company), PUT /companies/:companyId, DELETE /companies/:companyId, etc.
 
